6db087c7086bac00c204e89e65c85c91f508a64a
rithickjackson8 <rithickjackson8@gmail.com>
2026-06-16 19:00:59 +0530

    git commit -m "feat: spring boot project setup with dependencies and configuration"

---
+++ src/main/java/com/foodbillpro/FoodBillProApplication.java
@@ -0,0 +1,11 @@
+package com.foodbillpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBillProApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBillProApplication.class, args);
+	}
+}
